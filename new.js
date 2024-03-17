86e5283953ded0b534466935ca54da6658a69918
Priyanshu Ranjan <133768704+priyanshuranjan2024@users.noreply.github.com>
2024-03-17 13:09:39 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,10 +1,1 @@
-fetch("https://kodessphere-api.vercel.app/devices", {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ teamid: "3WNAni7", device: "bulb", value: 1 }),
-})
-  .then((response) => response.json())
-  .then((response) => console.log(JSON.stringify(response)));
+
